bb986391605dee47add676fe84d890613cfdc3b6
Rudransh Soni <137506470+Rudransh-Soni@users.noreply.github.com>
2024-05-29 12:34:41 +0000

    script.js

--- Project-2(BMI Calculator)/script.js
+++ Project-2(BMI Calculator)/script.js
@@ -1,42 +1,22 @@
-// const form = document.querySelector('form')             // Here we choose to where we have to perform our event. here we have to perform our event on Submit Button.
+const form = document.querySelector('form')     // Here we choose to where we have to perform our event. here we have to perform our event on Submit Button.
 
-// form.addEventListener('submit', function(e){           // here we just create one 'submit
-//     e.preventDefault()
-
-//     const height = parseInt(document.querySelector("#Height").value)         // here we use parseInt to convet the values into Integer.
-//     const weight = parseInt(document.querySelector("#Weight").value)         
-//     const results = document.querySelector("#results")         
-
-//     if (height === '' || height < 0 || isNaN(height)) {
-//         results.innerHTML = `Plese give a valid height ${height}`
-//     }   else if (weight === '' || weight < 0 || isNaN(weight)) {
-//         results.innerHTML = `Plese give a valid weight ${weight}`
-//     } else{
-
-//     }
-// })
-
-
-
-const form = document.querySelector('form')
-
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function(e){     // here we just create one 'submit'.
     e.preventDefault()
 
-    const height= parseInt(document.querySelector('#Height').value)
-    const weight= parseInt(document.querySelector('#Weight').value)
+    const height= parseInt(document.querySelector('#Height').value)         // here we use parseInt to convet the values into Integer.
+    const weight= parseInt(document.querySelector('#Weight').value)         // here we use parseInt to convet the values into Integer.
     const result = document.querySelector('#results')
 
-    if(height === '' || height < 0 || isNaN(height)){
+    if(height === '' || height < 0 || isNaN(height)){               // here we just paas the condition of 'height' 
         result.innerHTML = `Please give a valid height ${height}`
-    } else if(weight === '' || weight < 0 || isNaN(weight)){
+    } else if(weight === '' || weight < 0 || isNaN(weight)){        // here we just paas the condition of 'weight'
         result.innerHTML = `Please give a valid weight ${weight}`
     }else{
-        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);        // here we give the gormula of BMI
         result.innerHTML = `<span>${bmi}</span>`
     }
 
-    // if('bmi < 18.6' ){
+    // if('bmi < 18.6' ){                           // Try cases.
     //     result.innerHTML = 'You are UnderWeight'
     // } else if ('bmi < 18.6' || 'bmi > 24.9'){
     //     result.innerHTML = "You are normal Range"
